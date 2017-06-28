8e5303208ca578e2389859a14c312e8f41e158dc
Nevenka3 <julie.genest@edu.itescia.Fr>
2017-06-28 20:58:32 +0200

    Loop foreach

--- rep-connectedDevices.php
+++ rep-connectedDevices.php
@@ -43,25 +43,24 @@
 <?php 
 
 	$result = shell_exec("arp"); //result of the command line
-	$tabResult = explode(" ", $result);	//put in an array the result	
+	$tabResult = explode("\t", $result);	//put in an array the result	
 	
 	$nbColumn = 6;
 	$i = 0; // init of $i
 
 	foreach($tabResult as $val){
-		if (!empty($val)){
-			if(explode(" ", $val)){
-				$values = explode(" ", $val);
-				for($j=0;$j<sizeof($values);$j++){
-					$tabFinal[$i] = $values[$j];
-					$i++;
-				}
-			}else{
-				$tabFinal[$i] = $val; //add the value in the new array
+		if(explode("\n", $val)){
+			$values = explode("\n", $val);
+			for($j=0;$j<sizeof($values);$j++){
+				$tabFinal[$i] = $values[$j];
 				$i++;
-			}				
-		}
+			}
+		}else{
+			$tabFinal[$i] = $val; //add the value in the new array
+			$i++;
+		}				
 	}
+
 	
 	$count = sizeof($tabFinal); //size of the new array
 	
