52dbf2fdccb0fd2a1e026315bf19db60cecbc768
Nevenka3 <julie.genest@edu.itescia.Fr>
2017-06-28 13:53:16 +0200

    Fichier log

--- library/exten-radius_log.php
+++ library/exten-radius_log.php
@@ -26,7 +26,7 @@
  
 
 $logfile_loc = array();
-$logfile_loc[1] = '../var/log/radius/radius.log';
+$logfile_loc[1] = $_SERVER['DOCUMENT_ROOT'].'/var/log/radius/radius.log';
 
 foreach ($logfile_loc as $tmp) {
 	if (file_exists($tmp)) { 
